85603ddf39902bfe3d46902ecc1d4e38b37ab156
ramya <educate1996@gmail.com>
2017-01-28 23:15:49 +0530

    working on geo_location_screen

--- geo_location_screen/admin.py
+++ geo_location_screen/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from django.db import models
 # Register your models here.
+class geo_location_dataAdmin(admin.ModelAdmin):
+	list_display=('marker','latitude','longitude')
+
