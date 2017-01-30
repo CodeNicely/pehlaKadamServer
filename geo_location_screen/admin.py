c0d5efcefbdae77b99b97153811bb63964392a3d
ramya <educate1996@gmail.com>
2017-01-30 20:40:06 +0530

    fixed models issue

--- geo_location_screen/admin.py
+++ geo_location_screen/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.db import models
+from .models import *
 # Register your models here.
 class geo_location_dataAdmin(admin.ModelAdmin):
-	list_display=('marker','latitude','longitude')
+	list_display=["marker","latitude","longitude"]
+admin.site.register(geo_location_data,geo_location_dataAdmin)
 
